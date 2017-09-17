a63c65eb26d5752173b7b97fcbe7e7137e2a5599
Glenn Smith <couleeapps@gmail.com>
2017-09-17 18:58:56 -0400

    Level Select UI

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+
+	public GameObject buttonPrefab;
+	RectTransform t;
+
+	// Use this for initialization
+	void Start() {
+		LevelManager manager = LevelManager.currentManager;
+		t = GetComponent<RectTransform>();
+
+		for (int i = 0; i < manager.levelNames.Length; i++) {
+			string name = manager.levelNames[i];
+
+			GameObject levelButton = Instantiate(buttonPrefab);
+			levelButton.transform.SetParent(t);
+			int levelNum = i;
+
+			RectTransform buttonTrans = levelButton.GetComponent<RectTransform>();
+			levelButton.GetComponent<Button>().onClick.AddListener(() => {
+				manager.LoadLevel(levelNum);
+			});
+			levelButton.GetComponentInChildren<Text>().text = name;
+			buttonTrans.anchoredPosition = new Vector3(0, -20 - (i * (buttonTrans.rect.height + 10)), 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
